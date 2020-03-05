f04d2808ca5cd7bf7720b7bc4142189c18c53de9
jheymejia <alexmejia799@gmail.com>
2020-03-05 17:03:37 -0500

    Ajuste en ventana de módulos

---
+++ app/mods/clientes/actualizacion.php
@@ -0,0 +1,23 @@
+<html>
+
+<head>
+  <title> SYSPOSW </title>
+</head>
+
+<body>
+
+  <!-- formulario para actualizar datos de clientes -->
+
+  <form action="               " method="post">
+    <ul>
+      <li>
+        <label for="name">IDENTIFICACIÓN:</label>
+        <input type="text" id="name" name="user_name">
+      </li>
+      <li class="button">
+        <button type="submit">BUSCAR</button>
+      </li>
+    </ul>
+</body>
+
+</html>
---
+++ app/mods/clientes/eliminarclientes.php
@@ -0,0 +1,27 @@
+<html>
+
+<head>
+  <title> SYSPOSW </title>
+</head>
+
+<body>
+
+  <!-- formulario para eliminar clientes -->
+
+  <form action="               " method="post">
+    <ul>
+      <li>
+        <label for="name">IDENTIFICACIÓN:</label>
+        <input type="text" id="name" name="user_name">
+      </li>
+      <li>
+        <label for="name">E-MAIL:</label>
+        <input type="text" id="name" name="user_name">
+      </li>
+      <li class="button">
+        <button type="submit">BUSCAR</button>
+      </li>
+    </ul>
+</body>
+
+</html>
