635143aa834883fb5103ed2821ec473c93ea1c6d
MolXSENA <58195118+MolXSENA@users.noreply.github.com>
2020-04-01 12:46:24 -0500

    Update conexion.php

--- datos/conexion.php
+++ datos/conexion.php
@@ -1,7 +1,7 @@
 <?php
 $servidor = "localhost";
 $servuser = "root";
-$password = "ymvaRRhkbAJ82Wlv";
+$password = "";
 $db = "sisposw89";
 $conexion = new mysqli($servidor, $servuser, $password, $db);
 
