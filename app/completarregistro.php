09e7c6ead3d93e6bfe82416263969d4ce782a424
Camilo Quiceno <juank.jcq32@gmail.com>
2020-04-02 17:14:59 -0500

    redir del index

--- app/completarregistro.php
+++ app/completarregistro.php
@@ -1,14 +1,14 @@
 <?php
 require "../datos/conexion.php";
 
-$nombre = $_POST['nom'];
-$apellido = $_POST['ape'];
-$direccion = $_POST['dir'];
-$celular = $_POST['cel'];
-$telefono = $_POST['tel'];
-$tipo = $_POST['tipo'];
-$identificacion = $_POST['iden'];
-
-$regis = new conexion;
-$regis->llenardatos($nombre,$apellido,$direccion,$celular,$telefono,$tipo,$identificacion);
-$regis->cerrarconex();
+  $nombre = $_POST['nom'];
+  $apellido = $_POST['ape'];
+  $direccion = $_POST['dir'];
+  $celular = $_POST['cel'];
+  $telefono = $_POST['tel'];
+  $tipo = $_POST['tipo'];
+  $identificacion = $_POST['iden'];
+  
+  $regis = new conexion;
+  $regis->llenardatos($nombre,$apellido,$direccion,$celular,$telefono,$tipo,$identificacion);
+  $regis->cerrarconex();
