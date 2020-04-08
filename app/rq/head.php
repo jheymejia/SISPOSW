30c24751ba4d5d8258b0b3c2587b9cf0a1f00de8
Jheyman Mejia <48772215+jheymejia@users.noreply.github.com>
2020-04-08 15:11:37 -0500

    Update head.php

--- app/rq/head.php
+++ app/rq/head.php
@@ -2,10 +2,11 @@
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="shortcut icon" href="src/img/favicon.png">
 <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-<link type="text/css" rel="stylesheet" href="src/css/materialize.css">
+<link type="text/css" rel="stylesheet" href="src/css/mdb.min.css">
 <link type="text/css" rel="stylesheet" href="src/css/bootstrap.min.css">
 <link type="text/css" rel="stylesheet" href="src/css/normalize.min.css">
 <link type="text/css" rel="stylesheet" href="src/css/menu.css">
 <link type="text/css" rel="stylesheet" href="src/css/main.css">
 
 <script type="text/javascript" src="src/js/jquery.min.js"></script>
+
