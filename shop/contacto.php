06feef3a95a3d4763bbb2fd42a938cd83d8754d9
jheymejia <alexmejia799@gmail.com>
2020-04-16 15:37:26 -0500

    Grilla de productos corregida

--- shop/contacto.php
+++ shop/contacto.php
@@ -5,7 +5,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="x-ua-compatible" content="ie=edge">
-	<title>Contact- SysPOSw</title>
+	<title>Contacto - SysPOSw</title>
 	<link rel="shortcut icon" href="img/favicon.png">
 	<meta name="description" content="">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
